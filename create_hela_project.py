ac5d0050145899b840650b85a7452054203850db
Constantin Pape <c.pape@gmx.net>
2021-07-22 14:29:04 +0200

    Add endo_pred

--- create_hela_project.py
+++ create_hela_project.py
@@ -8,7 +8,10 @@ def create_hela_project():
     # add the raw data
     clims = [2849, 6012]  # determined qualitatively
     raw_view = mobie.metadata.get_default_view("image", "fibsem-uint16", contrastLimits=clims)
-    mobie.add_open_organelle_data(address, root, source_name="em/fibsem-uint16", view=raw_view, overwrite=True)
+    mobie.add_open_organelle_data(address, root, internal_path="em/fibsem-uint16", view=raw_view)
+
+    # add "endo_pred"
+    mobie.add_open_organelle_data(address, root, internal_path="labels/endo_pred")
 
 
 if __name__ == '__main__':
